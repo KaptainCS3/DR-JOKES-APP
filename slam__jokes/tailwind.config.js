ae59451e7ecb4de39efa6839ae5d1196ac4f3b56
KaptainCS3 <mbiapplegryn@gmail.com>
2023-02-15 22:42:35 +0100

    config media query screen sizes

--- slam__jokes/tailwind.config.js
+++ slam__jokes/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      sm: { min: "200px", max: "475px" },
+      // => @media (min-width: 200px and max-width: 475px) { ... }
+
+      md: { min: "476px", max: "768px" },
+      // => @media (min-width: 476px and max-width: 768px) { ... }
+
+      lg: { min: "769px" },
+      // => @media (min-width: 769px ) { ... }
+    },
   },
   plugins: [],
-}
+};
