f1af7630721d19b88b4a70f88ac46ecc581172a1
KaptainCS3 <mbiapplegryn@gmail.com>
2023-02-16 22:24:07 +0100

    minor changes

--- slam__jokes/src/components/Nav.js
+++ slam__jokes/src/components/Nav.js
@@ -1,20 +1,57 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
 const Nav = () => {
-  const [width, setWidth] = useState(0);
-  const navBar = useRef();
-  const toggle = () => {};
+  const [isNavBar, setIsNavBar] = useState(false);
+  const showNav = () => {
+    setIsNavBar(true);
+    const style = {
+      display: "block",
+    };
+  };
+  const hideNav = () => {
+    setIsNavBar(false);
+    const style = {
+      display: "none",
+    };
+  };
+  console.log(isNavBar);
   return (
-    <nav
-      onClick={toggle}
-      ref={navBar}
-      className="nav sm:flex justify-between items-center h-10 w-full px-4 border md:w-3/4 border lg:w-48 border"
-    >
+    <nav className="bg-[#f00] nav sm:bg-[#6200EE] flex justify-between items-center h-12 w-full px-4 md:w-full flex lg:w-48">
       <div className="hamburger__menu">
-        <FontAwesomeIcon icon={faBars} />
+        <FontAwesomeIcon
+          icon={faBars}
+          className="text-2xl text-white cursor-pointer"
+          onClick={showNav}
+        />
       </div>
-      <p>Logo {width}</p>
+      <h1>
+        <span className="text-xl text-white">Slam</span>{" "}
+        <span className="text-xl text-white">Jokes</span>
+        <sup className="text-xl text-white">++</sup>
+      </h1>
+      {/* "sm:absolute top-0 left-0 w-[70%] h-full bg-[#6200EE] text-white" */}
+      <div
+        className={
+          isNavBar
+            ? "sm:absolute top-0 left-0 w-[70%] h-full bg-[#6200EE] text-white"
+            : "hidden"
+        }
+      >
+        <div className="hamburger__menu pt-2 pl-4">
+          <FontAwesomeIcon
+            icon={faClose}
+            className="text-3xl cursor-pointer"
+            onClick={hideNav}
+          />
+          <div className="">
+            <p>Categories</p>
+            <p>Create Jokes</p>
+            <p>Theme</p>
+          </div>
+          <p>Settings</p>
+        </div>
+      </div>
     </nav>
   );
 };
