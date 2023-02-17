a27361896fa43b4ce6e46e898e364e3d5311eca8
KaptainCS3 <mbiapplegryn@gmail.com>
2023-02-17 22:24:25 +0100

    initial commit

---
+++ slam__jokes/src/components/JokeContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const JokeContainer = (props) => {
+  return (
+    <div className="flex justify-center items-center w-[95%] h-64 my-16 rounded-xl shadow-2xl flex-col">
+      <p></p>
+      <p></p>
+    </div>
+  );
+};
+
+export default JokeContainer;
