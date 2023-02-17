b2dead1406a34811f71f07f3d33bb7fa6814c039
KaptainCS3 <mbiapplegryn@gmail.com>
2023-02-17 22:24:47 +0100

    initial commit

---
+++ slam__jokes/src/components/Comment.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Comment = () => {
+  const focus = {
+    outline: "none",
+  };
+  return (
+    <form className="w-full h-[3rem] flex justify-between items-center px-2 rounded shadow">
+      <div className="py-[2.5rem w-[80%]">
+        <input
+          type="text"
+          placeholder="Leave a comment...."
+          className="py-[.7rem] w-full"
+          style={focus}
+        />
+      </div>
+      <div>
+        <button className="text-[#6200EE] font-bold w-[20%]">Post</button>
+      </div>
+    </form>
+  );
+};
+
+export default Comment;
