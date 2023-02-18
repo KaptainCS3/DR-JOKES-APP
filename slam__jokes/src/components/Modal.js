87c402c96e9c709890818cae1d922600d7c723e4
KaptainCS3 <mbiapplegryn@gmail.com>
2023-02-18 23:17:32 +0100

    minor changes

--- slam__jokes/src/components/Modal.js
+++ slam__jokes/src/components/Modal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import JokeContainer from "./JokeContainer";
 
 const Modal = () => {
   const [showModal, setShowModal] = React.useState(false);
@@ -6,11 +7,11 @@ const Modal = () => {
     <div className="md:block sm:hidden lg:hidden">
       <>
         <button
-          className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          className="text-dark font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
           type="button"
           onClick={() => setShowModal(true)}
         >
-          Open regular modal
+          Click me!!!!!!!!
         </button>
         {showModal ? (
           <>
