1aa67549aeb4e3c3086cd007955a0bf9fc802a2d
KaptainCS3 <mbiapplegryn@gmail.com>
2023-02-18 23:18:09 +0100

    initial commit

---
+++ slam__jokes/src/jokes.js
@@ -0,0 +1,54 @@
+const jokes = [
+  {
+    id: 1,
+    punchline: "some random text",
+    body: "this is the content of the joke do you wish to add anything silly to it guest no!!",
+  },
+  {
+    id: 2,
+    punchline: "some random text",
+    body: "this is the content of the joke do you wish to add anything silly to it guest no!!",
+  },
+  {
+    id: 3,
+    punchline: "some random text",
+    body: "this is the content of the joke do you wish to add anything silly to it guest no!!",
+  },
+  {
+    id: 4,
+    punchline: "some random text",
+    body: "this is the content of the joke do you wish to add anything silly to it guest no!!",
+  },
+  {
+    id: 5,
+    punchline: "some random text",
+    body: "this is the content of the joke do you wish to add anything silly to it guest no!!",
+  },
+  {
+    id: 6,
+    punchline: "some random text",
+    body: "this is the content of the joke do you wish to add anything silly to it guest no!!",
+  },
+  {
+    id: 7,
+    punchline: "some random text",
+    body: "this is the content of the joke do you wish to add anything silly to it guest no!!",
+  },
+  {
+    id: 8,
+    punchline: "some random text",
+    body: "this is the content of the joke do you wish to add anything silly to it guest no!!",
+  },
+  {
+    id: 9,
+    punchline: "some random text",
+    body: "this is the content of the joke do you wish to add anything silly to it guest no!!",
+  },
+  {
+    id: 10,
+    punchline: "some random text",
+    body: "this is the content of the joke do you wish to add anything silly to it guest no!!",
+  },
+];
+
+export default jokes
