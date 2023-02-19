1400351415ff51429d9417d2f0acd8d120e4981b
KaptainCS3 <mbiapplegryn@gmail.com>
2023-02-19 22:35:00 +0100

    minor changes

--- slam__jokes/src/App.js
+++ slam__jokes/src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Nav from "./components/Nav";
-import './App.css'
+import "./App.css";
+import Welcome from "./components/Welcome";
+import Jokes from "./components/Jokes";
+import CreateJoke from "./components/CreateJoke";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
 const App = () => {
   return (
-    <div>
+    <div className="lg:flex">
       <Nav />
+      <Welcome />
+      <Jokes />
+      <CreateJoke />
+      <Container />
+      <Container />
+      <Footer />
     </div>
   );
 };
