da96a839d121eef854ed0ab5ef8befbe6cec643f
KaptainCS3 <mbiapplegryn@gmail.com>
2023-02-19 22:37:39 +0100

    update component

--- slam__jokes/src/components/Nav.js
+++ slam__jokes/src/components/Nav.js
@@ -7,7 +7,10 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import moon from "../assets/icon-moon.svg";
 import sun from "../assets/icon-sun.svg";
+import Example from "./Example";
+import styles from "../styles/Nav.module.css";
 const Nav = () => {
+  // const categoriesData = categories
   const [isNavBar, setIsNavBar] = useState(false);
   const showNav = () => {
     setIsNavBar(true);
@@ -17,7 +20,7 @@ const Nav = () => {
   };
   console.log(isNavBar);
   return (
-    <nav className="bg-[#6200EE] nav sm:bg-[#6200EE] flex justify-between items-center h-12 w-full px-4 md:w-full flex lg:w-1/4">
+    <nav className="bg-[#6200EE] dark:bg-[#121212] nav sm:bg-[#6200EE] flex justify-between items-center h-12 w-full px-4 md:w-full py-10 flex lg:w-1/4">
       <div className="hamburger__menu md:hidden lg:hidden">
         <FontAwesomeIcon
           icon={faBars}
@@ -33,15 +36,19 @@ const Nav = () => {
       <div className="table__view w-[70%] text-white font-bold flex justify-between sm:hidden lg:hidden">
         <>
           <button>
-            Create Joke
+            <Example
+              width="25rem"
+              text="CREATE JOKE"
+              header="CREATE JOKE/RIDDLE"
+            />
             <FontAwesomeIcon icon={faChevronDown} className="pl-2" />
           </button>
           <button>
-            Categories
+            <Example width="30rem" text="CATEGORIES" header="CATEGORIES" />
             <FontAwesomeIcon icon={faChevronDown} className="pl-2" />
           </button>
           <button>
-            Settings
+            <Example width="25rem" text="SETTINGS" header="SETTINGS" />
             <FontAwesomeIcon icon={faChevronDown} className="pl-2" />
           </button>
         </>
@@ -65,14 +72,21 @@ const Nav = () => {
           />
           <div className="sm:flex flex-col md:hidden lg:hidden">
             <div className="mt-12">
-              <p className="pt-4">Categories</p>
+              {/* <p className="pt-4">Categories</p> */}
+              <Example />
               <p className="pt-4">Create Jokes</p>
               <p className="pt-4">
-                <label htmlFor="theme">Dark</label>
-                <input type="radio" id="theme" />
+                {/* <label htmlFor="theme">Dark</label>
+                <input type="radio" id="theme" /> */}
+                <label className={styles.toggleDarkBtn}>
+                  <input type="checkbox" />
+                  <span
+                    className={`${styles.slideBtnTg} ${styles.round}`}
+                  >Theme</span>
+                </label>
               </p>
             </div>
-          <p className="mt-[20rem]">Settings</p>
+            <p className="mt-[20rem]">Settings</p>
           </div>
         </div>
       </div>
