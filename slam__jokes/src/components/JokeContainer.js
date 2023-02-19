6004648456761354a7f3468790075415de1359f3
KaptainCS3 <mbiapplegryn@gmail.com>
2023-02-19 22:36:19 +0100

    minor changes

--- slam__jokes/src/components/JokeContainer.js
+++ slam__jokes/src/components/JokeContainer.js
@@ -2,10 +2,15 @@ import React from "react";
 
 const JokeContainer = (props) => {
   return (
-    <div className="flex justify-center items-center w-[95%] h-64 my-16 rounded-xl shadow-2xl flex-col">
-      <p></p>
-      <p></p>
-    </div>
+    <article
+      onClick={props.toggle}
+      className={`text-center py-[1.5rem] py-[1.5rem] px-[2rem] shadow-2xl rounded-xl h-64 flex justify-center flex-col md:w-96 mr-8 mb-8`}
+    >
+      <h4 className={`mb-[0.25rem] font-bold`}>{props.punchline}</h4>
+      <p className={`mb-[0.75rem]`}>
+        {props.body} {props.id}
+      </p>
+    </article>
   );
 };
 
