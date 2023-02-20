8a34d89184575e4669ebcdf4c4d79d1932da3dba
KaptainCS3 <mbiapplegryn@gmail.com>
2023-02-20 22:34:28 +0100

    initial commit

---
+++ slam__jokes/src/components/ChevronLeft.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+const ChevronLeft = (props) => {
+  return (
+    <>
+      {/* chevron left // prev joke Indicator */}
+      <div
+        className={`dark:text-slate-400 arrow__indicator left mt-2 md:absolute -left-[2%] top-[50%]`}
+      >
+        <FontAwesomeIcon
+          icon={faChevronLeft}
+          className={`cursor-pointer text-2xl`}
+          onClick={props.prevJoke}
+        />
+      </div>
+    </>
+  );
+};
+
+export default ChevronLeft;
