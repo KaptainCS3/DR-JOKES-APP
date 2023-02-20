b2e594b4716dcfc51c088076b787f3908d0cfe48
KaptainCS3 <mbiapplegryn@gmail.com>
2023-02-20 22:51:41 +0100

    lift state one level up

--- slam__jokes/src/App.js
+++ slam__jokes/src/App.js
@@ -1,20 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./components/Nav";
 import "./App.css";
 import Welcome from "./components/Welcome";
 import Jokes from "./components/Jokes";
 import CreateJoke from "./components/CreateJoke";
-import Container from "./components/Container";
 import Footer from "./components/Footer";
+import { DarkModeSwitch } from "react-toggle-dark-mode";
+import useDarkMode from "./hooks/useDarkMode";
+import JokeSlider from "./components/JokeSlider";
+
 const App = () => {
+  const [colorTheme, setColorTheme] = useDarkMode();
+  const [isDarkMode, setDarkMode] = useState(
+    colorTheme === "light" ? true : false
+  );
+  const toggleDarkMode = (checked) => {
+    setColorTheme(colorTheme);
+    setDarkMode(checked);
+  };
   return (
-    <div className="lg:flex">
-      <Nav />
-      <Welcome />
-      <Jokes />
-      <CreateJoke />
-      <Container />
-      <Container />
+    <div className="lg:flex min-h-[100vh]">
+      <Nav
+        colorTheme={colorTheme}
+        setColorTheme={setColorTheme}
+        isDarkMode={isDarkMode}
+        setDarkMode={setColorTheme}
+        toggleDarkMode={toggleDarkMode}
+      />
+      <main className="dark:bg-[#121212] lg:w-4/5">
+        <div className="lg:flex justify-between items-center h-16 px-4">
+          <Welcome />
+          <DarkModeSwitch
+            className="sm:hidden md:hidden"
+            size={35}
+            checked={isDarkMode}
+            onChange={toggleDarkMode}
+          />
+        </div>
+        <Jokes />
+        <CreateJoke />
+        <div>
+          <JokeSlider />
+          <JokeSlider />
+        </div>
+      </main>
       <Footer />
     </div>
   );
