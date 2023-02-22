31b7b7db2a1c331d89a812518944f58ab5dddf6e
KaptainCS3 <mbiapplegryn@gmail.com>
2023-02-22 13:18:45 +0100

    comment reducer

--- slam__jokes/src/app/store.js
+++ slam__jokes/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "../features/themeSlice";
+import commentReducer from "../features/commentSlice";
 export const store = configureStore({
   reducer: {
     // reaction: reactionReducer,
     // createJoke: createJokeReducer,
     toggleTheme: themeReducer,
+    jokeComment: commentReducer,
   },
 });
