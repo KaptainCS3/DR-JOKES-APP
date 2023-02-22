6a9bdc4a1fda116f8985ef557a54c1e1ce189ddf
KaptainCS3 <mbiapplegryn@gmail.com>
2023-02-22 13:13:05 +0100

    lift state to app component

--- slam__jokes/src/App.js
+++ slam__jokes/src/App.js
@@ -8,15 +8,21 @@ import Footer from "./components/Footer";
 import { DarkModeSwitch } from "react-toggle-dark-mode";
 import useDarkMode from "./hooks/useDarkMode";
 import JokeSlider from "./components/JokeSlider";
-
 const App = () => {
   const [colorTheme, setColorTheme] = useDarkMode();
+  //select state
+  const [selectCategory, setSelectCategory] = useState("African Jokes");
   const [isDarkMode, setDarkMode] = useState(
     colorTheme === "light" ? true : false
   );
+
   const toggleDarkMode = (checked) => {
     setColorTheme(colorTheme);
     setDarkMode(checked);
+  };
+  //track select value change
+  const handleChange = (e) => {
+    setSelectCategory(e.target.value);
   };
   return (
     <div className="lg:flex min-h-[100vh]">
@@ -26,6 +32,9 @@ const App = () => {
         isDarkMode={isDarkMode}
         setDarkMode={setColorTheme}
         toggleDarkMode={toggleDarkMode}
+        selectCategory={selectCategory}
+        setSelectCategory={setSelectCategory}
+        handleChange={handleChange}
       />
       <main className="dark:bg-[#121212] lg:w-4/5">
         <div className="lg:flex justify-between items-center h-16 px-4">
@@ -37,7 +46,7 @@ const App = () => {
             onChange={toggleDarkMode}
           />
         </div>
-        <Jokes />
+        <Jokes selectCategory={selectCategory} />
         <CreateJoke />
         <div>
           <JokeSlider />
