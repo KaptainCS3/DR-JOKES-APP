90136ed0c9100dad4a1a5057634157789e3f19f1
KaptainCS3 <mbiapplegryn@gmail.com>
2023-02-23 23:21:04 +0100

    remove medium and large screen style

--- slam__jokes/src/components/ChevronLeft.js
+++ slam__jokes/src/components/ChevronLeft.js
@@ -6,7 +6,7 @@ const ChevronLeft = (props) => {
     <>
       {/* chevron left // prev joke Indicator */}
       <div
-        className={`dark:text-slate-400 arrow__indicator left mt-2 md:absolute -left-[1%] top-[50%] lg:absolute left-[5%] top-[45%]`}
+        className={`dark:text-slate-400 arrow__indicator left mt-2`}
       >
         <FontAwesomeIcon
           icon={faChevronLeft}
