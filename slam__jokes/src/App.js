a52ea34ad8212f4c4b01c0f93159fef32aa574df
KaptainCS3 <mbiapplegryn@gmail.com>
2023-02-24 16:40:31 +0100

    lift state to App component

--- slam__jokes/src/App.js
+++ slam__jokes/src/App.js
@@ -10,6 +10,12 @@ import useDarkMode from "./hooks/useDarkMode";
 import JokeSlider from "./components/JokeSlider";
 import catValue from "./categoryValue";
 const App = () => {
+  //!Show modal state
+  const [showModal, setShowModal] = useState(false);
+
+  //!Nav display state
+  const [isNavBar, setIsNavBar] = useState(false);
+
   //! Theme color State
   const [colorTheme, setColorTheme] = useDarkMode();
 
@@ -18,21 +24,68 @@ const App = () => {
     colorTheme === "light" ? true : false
   );
 
+  //!Display create joke
+  const [displayCreateJoke, setDisplayCreateJoke] = useState(false);
+  const handleClick = () => {
+    setDisplayCreateJoke(true);
+  };
+  const closeCreateJoke = () => {
+    setDisplayCreateJoke(false);
+  };
+
+  //! category index state
+  const [catIndex, setCatIndex] = useState(0);
+
+  //!track input create form
+  //! select joke category state
+  const [createJokeCategory, setCreateJokeCategory] = useState("African Jokes");
+
+  //! form data input joke
+  const [createJokeData, setCreateJokeData] = useState({
+    punchline: "",
+    body: "",
+    author: "",
+    time: "",
+    jokeCategory: createJokeCategory,
+  });
+
+  //!selected category state
+  const [selectCategory, setSelectCategory] = useState("African Jokes");
+
   //! Dark mode handler
   const toggleDarkMode = (checked) => {
     setColorTheme(colorTheme);
     setDarkMode(checked);
   };
 
-  //!selected category state
-  const [selectCategory, setSelectCategory] = useState("African Jokes");
-
-  //! category index state
-  const [catIndex, setCatIndex] = useState(0);
-
   //!track selected category value change
   const handleChange = (e) => {
     setSelectCategory(e.target.value);
+  };
+
+  const createJokeHandleChange = (event) => {
+    const { name, value } = event.target;
+    setCreateJokeData((prevCreateJokeData) => {
+      return {
+        ...prevCreateJokeData,
+        [name]: value,
+      };
+    });
+  };
+  //! prevent page reload
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setCreateJokeData({
+      punchline: "",
+      body: "",
+      author: "",
+      time: "",
+      jokeCategory: createJokeCategory,
+    });
+  };
+
+  const handleCreateDataChange = (e) => {
+    setCreateJokeCategory(e.target.value);
   };
   return (
     <div className="lg:flex min-h-[100vh]">
@@ -45,10 +98,15 @@ const App = () => {
         selectCategory={selectCategory}
         setSelectCategory={setSelectCategory}
         handleChange={handleChange}
+        handleClick={handleClick}
+        displayCreateJoke={displayCreateJoke}
+        setDisplayCreateJoke={setDisplayCreateJoke}
+        isNavBar={isNavBar}
+        setIsNavBar={setIsNavBar}
       />
       <main className="dark:bg-[#121212] lg:w-4/5">
         <div className="lg:flex justify-between items-center h-16 px-4">
-          <Welcome />
+          <Welcome selectCategory={selectCategory} />
           <DarkModeSwitch
             className="sm:hidden md:hidden"
             size={35}
@@ -62,19 +120,33 @@ const App = () => {
           catIndex={catIndex}
           setCatIndex={setCatIndex}
         />
-        <CreateJoke />
+        <CreateJoke
+          createJokeCategory={createJokeCategory}
+          createJokeData={createJokeData}
+          handleSubmit={handleSubmit}
+          createJokeHandleChange={createJokeHandleChange}
+          handleCreateDataChange={handleCreateDataChange}
+          displayCreateJoke={displayCreateJoke}
+          setDisplayCreateJoke={setDisplayCreateJoke}
+          handleClick={handleClick}
+          closeCreateJoke={closeCreateJoke}
+        />
         <div>
           <JokeSlider
             selectCategory={selectCategory}
             catValue={catValue}
             catIndex={catIndex}
             setCatIndex={setCatIndex}
+            showModal={showModal}
+            setShowModal={setShowModal}
           />
           <JokeSlider
             selectCategory={selectCategory}
             catValue={catValue}
             catIndex={catIndex}
             setCatIndex={setCatIndex}
+            showModal={showModal}
+            setShowModal={setShowModal}
           />
         </div>
       </main>
