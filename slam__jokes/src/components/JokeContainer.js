c3247ba652c729b9027a13fd26e506279dec0726
KaptainCS3 <mbiapplegryn@gmail.com>
2023-02-25 21:45:10 +0100

    minor changes

--- slam__jokes/src/components/JokeContainer.js
+++ slam__jokes/src/components/JokeContainer.js
@@ -7,7 +7,7 @@ const JokeContainer = (props) => {
       className={`cursor-pointer dark:bg-slate-400 sm:px-[2rem] mr-0 text-center py-[1.5rem] shadow-2xl rounded-xl h-64 flex justify-center items-center w-full flex-col md:mr-10 w-[20rem] px-4 mb-8 lg:mr-10`}
     >
       <h4 className={`mb-[0.25rem] font-bold`}>{props.punchline}</h4>
-      <p className={`mb-[0.75rem]`}>{props.body}</p>
+      <p className={`mb-[0.75rem]`}>{props.setup}</p>
     </article>
   );
 };
