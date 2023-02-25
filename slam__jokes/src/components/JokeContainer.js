9c73e2c51390a84e28dd67f4d6408ba45dd192b8
KaptainCS3 <mbiapplegryn@gmail.com>
2023-02-25 21:44:54 +0100

    minor changes

--- slam__jokes/src/components/JokeContainer.js
+++ slam__jokes/src/components/JokeContainer.js
@@ -4,12 +4,10 @@ const JokeContainer = (props) => {
   return (
     <article
       onClick={props.toggle}
-      className={`cursor-pointer dark:bg-slate-400 sm:px-[2rem] mr-0 text-center py-[1.5rem] shadow-2xl rounded-xl h-64 flex justify-center items-center w-full flex-col md:mr-10 px-32 mb-8 w-full lg:mr-10`}
+      className={`cursor-pointer dark:bg-slate-400 sm:px-[2rem] mr-0 text-center py-[1.5rem] shadow-2xl rounded-xl h-64 flex justify-center items-center w-full flex-col md:mr-10 w-[20rem] px-4 mb-8 lg:mr-10`}
     >
       <h4 className={`mb-[0.25rem] font-bold`}>{props.punchline}</h4>
-      <p className={`mb-[0.75rem]`}>
-        and category is {props.category.name} with id: {props.id}
-      </p>
+      <p className={`mb-[0.75rem]`}>{props.body}</p>
     </article>
   );
 };
