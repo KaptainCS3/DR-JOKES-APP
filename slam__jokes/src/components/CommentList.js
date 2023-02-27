d5fb3dea797e907f243e9c477cdefb59d88f23ee
KaptainCS3 <mbiapplegryn@gmail.com>
2023-02-27 16:05:58 +0100

    implement comment list

--- slam__jokes/src/components/CommentList.js
+++ slam__jokes/src/components/CommentList.js
@@ -1,12 +1,6 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import Comment from "./Comment";
-const CommentList = () => {
-  const commentList = useSelector((state) => state.jokeComment);
-  const list = commentList.map((e) => {
-    return <Comment key={e.id} {...e} />;
-  });
-  return <div>{list}</div>;
+const CommentList = (props) => {
+  return <div className="w-full my-4">{props.comments}</div>;
 };
 
 export default CommentList;
