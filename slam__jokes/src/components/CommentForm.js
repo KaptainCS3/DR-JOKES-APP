18768ca7e249578a844657ddf886d5dfef931579
KaptainCS3 <mbiapplegryn@gmail.com>
2023-02-28 21:08:09 +0100

    implement post comment no form validation yet

--- slam__jokes/src/components/CommentForm.js
+++ slam__jokes/src/components/CommentForm.js
@@ -1,67 +1,22 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { nanoid } from "@reduxjs/toolkit";
-import { postComment } from "../features/commentSlice";
-const CommentForm = () => {
-  const focus = {
-    outline: "none",
-  };
-
-  const dispatch = useDispatch();
-
-  //input state
-  const [addComment, setAddComment] = useState({
-    addComment: "",
-  });
-  //prevent page reload on submit
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setAddComment({ addComment: "" });
-  };
-
-  // track input value changes
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setAddComment((prevComment) => {
-      return {
-        ...prevComment,
-        [name]: value,
-      };
-    });
-  };
-
-  //add comment to joke
-  const handlePost = () => {
-    dispatch(
-      postComment({
-        id: nanoid(),
-        comment: addComment.addComment,
-      })
-    );
-  };
+import React from "react";
+const CommentForm = (props) => {
   return (
     <form
-      onSubmit={handleSubmit}
+      onSubmit={props.submitJokeComment}
       className="w-full h-[3rem] flex justify-between items-center px-2 rounded shadow bg-white mt-5"
     >
       <div className="py-[2.5rem] w-[80%]">
         <input
           type="text"
           placeholder="Leave a comment...."
-          className="py-[.7rem] w-full"
-          style={focus}
-          name="addComment"
-          onChange={handleChange}
-          value={addComment.addComment}
+          className="py-[.7rem] w-full focus:outline-none"
+          name="comment"
+          onChange={props.handleChangeComment}
+          value={props.comment.comment}
         />
       </div>
       <div>
-        <button
-          onClick={handlePost}
-          className="text-[#6200EE] font-bold w-[20%]"
-        >
-          Post
-        </button>
+        <button className="text-[#6200EE] font-bold w-[20%]">Post</button>
       </div>
     </form>
   );
