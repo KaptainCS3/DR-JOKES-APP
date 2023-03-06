647258b52ce7d6c153874c719774fa8abc678389
KaptainCS3 <mbiapplegryn@gmail.com>
2023-03-06 18:56:50 +0100

    minor changes

--- slam__jokes/src/components/Welcome.js
+++ slam__jokes/src/components/Welcome.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Welcome = (props) => {
-  return <div className="text-2xl dark:text-white">Enjoy and discover</div>;
+  return (
+    <div className="text-2xl sm:text-center pt-8 dark:text-white">
+      Enjoy and discover
+    </div>
+  );
 };
 
 export default Welcome;
