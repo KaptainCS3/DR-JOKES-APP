5aadc03bfe3506b9dc66e9c514030c15416b0dc4
KaptainCS3 <mbiapplegryn@gmail.com>
2023-03-06 18:55:11 +0100

    minor changes

--- slam__jokes/src/components/Comment.js
+++ slam__jokes/src/components/Comment.js
@@ -2,7 +2,7 @@ import React from "react";
 const Comment = (props) => {
   return (
     <article
-      className={`cursor-pointer dark:bg-slate-400 sm:w-full px-[1.5rem] mr-0 my-4 py-[0.5rem] shadow rounded-xl flex flex-col`}
+      className={`bg-white cursor-pointer dark:bg-slate-400 sm:w-full px-[1.5rem] mr-0 my-4 py-[0.5rem] shadow rounded-xl flex flex-col`}
     >
       <h4 className={`font-bold`}>@{props.useName}</h4>
       <p className={``}>{props.comment}</p>
