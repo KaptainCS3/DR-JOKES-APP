1459b00ac6f717a95e67b87a00518d939fcd02bf
KaptainCS3 <mbiapplegryn@gmail.com>
2023-03-08 20:42:59 +0100

    update received props

--- slam__jokes/src/components/Comment.js
+++ slam__jokes/src/components/Comment.js
@@ -4,7 +4,7 @@ const Comment = (props) => {
     <article
       className={`bg-white cursor-pointer dark:bg-slate-400 sm:w-full px-[1.5rem] mr-0 my-4 py-[0.5rem] shadow rounded-xl flex flex-col`}
     >
-      <h4 className={`font-bold`}>@{props.useName}</h4>
+      <h4 className={`font-bold`}>@{props.userName}</h4>
       <p className={``}>{props.comment}</p>
     </article>
   );
