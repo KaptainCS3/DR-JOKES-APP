eea78933f29b4871d023b98fd726e484521127cc
KaptainCS3 <mbiapplegryn@gmail.com>
2023-03-08 20:44:48 +0100

    add animations

--- slam__jokes/src/components/Welcome.js
+++ slam__jokes/src/components/Welcome.js
@@ -1,10 +1,12 @@
 import React from "react";
-
+import Zoom from "react-reveal/Zoom";
 const Welcome = (props) => {
   return (
-    <div className="text-2xl sm:text-center pt-8 dark:text-white">
-      Enjoy and discover
-    </div>
+    <Zoom right cascade>
+      <div className="text-2xl sm:text-center pt-8 dark:text-white">
+        Enjoy and discover
+      </div>
+    </Zoom>
   );
 };
 
