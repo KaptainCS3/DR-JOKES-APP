21ec118808f0b70449eae590746a576aa9780d36
KaptainCS3 <mbiapplegryn@gmail.com>
2023-04-02 23:37:50 +0100

    remove used functions

--- slam__jokes/src/components/CreateJoke.js
+++ slam__jokes/src/components/CreateJoke.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClose, faPlus } from "@fortawesome/free-solid-svg-icons";
 import styles from "../styles/placeholder.module.css";
